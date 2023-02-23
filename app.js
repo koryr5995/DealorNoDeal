d28689d9cf8dc110301251dfb2f4dfb3524e12ce
Kory Reddock <koryreddock@Korys-MacBook-Pro.local>
2023-02-23 09:32:23 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,103 @@
+let briefAmounts = {
+    briefcase1: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase2: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase3: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase4: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase5: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase6: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase7: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase8: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase9: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase10: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase11: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase12: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase13: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase14: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase15: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase16: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase17: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase18: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase19: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+    briefcase20: {
+        briefcasevalue:1000,
+        url:"",
+        banner:"",
+    },
+}
+let bestScore = ((currentScore > previousScore) || (previousScore > currentScore))
