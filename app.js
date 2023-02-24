cbb5885b354a5200a91117a721fd30f83bdc50d9
Kory Reddock <koryreddock@Korys-MacBook-Pro.local>
2023-02-24 09:14:19 -0500

    GameProject

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let briefAmounts = {
+let briefP1Amounts = {
     briefcase1: {
         briefcasevalue:30000,
         url:"",
@@ -90,7 +90,7 @@ let briefAmounts = {
         banner:"",
     },
     briefcase19: {
-        briefcasevalue:1000,
+        briefcasevalue:170000,
         url:"",
         banner:"",
     },
@@ -100,4 +100,108 @@ let briefAmounts = {
         banner:"",
     },
 }
+
+let briefP2Amounts = {
+    briefcase1: {
+        briefcasevalue:10000,
+        url:"",
+        banner:"",
+    },
+    briefcase2: {
+        briefcasevalue:100000,
+        url:"",
+        banner:"",
+    },
+    briefcase3: {
+        briefcasevalue:140000,
+        url:"",
+        banner:"",
+    },
+    briefcase4: {
+        briefcasevalue:30000,
+        url:"",
+        banner:"",
+    },
+    briefcase5: {
+        briefcasevalue:60000,
+        url:"",
+        banner:"",
+    },
+    briefcase6: {
+        briefcasevalue:20000,
+        url:"",
+        banner:"",
+    },
+    briefcase7: {
+        briefcasevalue:180000,
+        url:"",
+        banner:"",
+    },
+    briefcase8: {
+        briefcasevalue:80000,
+        url:"",
+        banner:"",
+    },
+    briefcase9: {
+        briefcasevalue:40000,
+        url:"",
+        banner:"",
+    },
+    briefcase10: {
+        briefcasevalue:150000,
+        url:"",
+        banner:"",
+    },
+    briefcase11: {
+        briefcasevalue:190000,
+        url:"",
+        banner:"",
+    },
+    briefcase12: {
+        briefcasevalue:50000,
+        url:"",
+        banner:"",
+    },
+    briefcase13: {
+        briefcasevalue:160000,
+        url:"",
+        banner:"",
+    },
+    briefcase14: {
+        briefcasevalue:130000,
+        url:"",
+        banner:"",
+    },
+    briefcase15: {
+        briefcasevalue:110000,
+        url:"",
+        banner:"",
+    },
+    briefcase16: {
+        briefcasevalue:200000,
+        url:"",
+        banner:"",
+    },
+    briefcase17: {
+        briefcasevalue:90000,
+        url:"",
+        banner:"",
+    },
+    briefcase18: {
+        briefcasevalue:70000,
+        url:"",
+        banner:"",
+    },
+    briefcase19: {
+        briefcasevalue:170000,
+        url:"",
+        banner:"",
+    },
+    briefcase20: {
+        briefcasevalue:120000,
+        url:"",
+        banner:"",
+    },
+}
+
 let bestScore = ((currentScore > previousScore) || (previousScore > currentScore))
